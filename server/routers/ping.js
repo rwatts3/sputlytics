b2efb2c67e32cf9b2b47e4b67782eefc300aa1b3
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-03-16 21:53:17 -0300

    improving security inside /ping route

--- server/routers/ping.js
+++ server/routers/ping.js
@@ -1,5 +1,4 @@
 const url = Meteor.npmRequire("url")
-const queryString = Meteor.npmRequire("querystring")
 const userAgent = Meteor.npmRequire("user-agent-parser")
 const device = Meteor.npmRequire("user-agent-device-mapper")
 const geoip = Meteor.npmRequire("geoip-lite")
@@ -8,44 +7,39 @@ const helmet = Meteor.npmRequire("helmet")
 
 Picker.middleware(cors())
 Picker.middleware(helmet())
-Picker.middleware((req, res, next) => {
-  const referer = url.parse(req.headers.referer)
-  const query = queryString.parse(req._parsedUrl.query)
-  const domain = referer.host || null
-  const _id = query.k || null
-  if (!!Domains.findOne({_id: _id, domain: domain})) {
-    next()
-  } else {
-    res.writeHead(412)
-    res.end()
-  }
-})
 
 Picker.route("/ping", (params, req, res, next) => {
   const headers = req.headers
   const query = params.query
-  const ip = headers["x-forwarded-for"]
-  const geo = geoip.lookup(ip) || {}
-  const agent = userAgent(headers["user-agent"])
-  const path = url.parse(req.headers.referer).pathname
-  agent.device.type = device.getDeviceType(headers["user-agent"])
-  Visits.insert({
-    dkey: query.k || null,
-    path: path,
-    ip: ip,
-    geo: {
-      ll: geo.ll || null,
-      c: geo.country || null,
-      ct: geo.city || null,
-      rg: geo.region || null
-    },
-    ua: agent,
-    lang: query.l || null,
-    screen: {
-      w: +query.w || null,
-      h: +query.h || null,
-    }
-  })
-  res.writeHead(204)
+  const referer = headers.referer
+  const origin = url.parse(referer) || null
+  const dkey = query.k || null
+  if (!!Domains.findOne({_id: dkey, domain: origin.host})) {
+    const ip = headers["x-forwarded-for"]
+    const geo = geoip.lookup(ip) || {}
+    const agent = userAgent(headers["user-agent"])
+    const path = origin.pathname
+    agent.device.type = device.getDeviceType(headers["user-agent"])
+    Visits.insert({
+      dkey: dkey,
+      path: path,
+      ip: ip,
+      geo: {
+        ll: geo.ll,
+        c: geo.country,
+        ct: geo.city,
+        rg: geo.region
+      },
+      ua: agent,
+      lang: query.l,
+      screen: {
+        w: +query.w,
+        h: +query.h
+      }
+    })
+    res.writeHead(204)
+  } else {
+    res.writeHead(412)
+  }
   res.end()
 })
