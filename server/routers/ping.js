24b7ff88500c9dc3559d0c72e0fc53fc5a447507
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-03-16 21:14:45 -0300

    improve security to block invalid domain and domainKey

--- server/routers/ping.js
+++ server/routers/ping.js
@@ -1,44 +1,49 @@
 const url = Meteor.npmRequire("url")
+const queryString = Meteor.npmRequire("querystring")
 const userAgent = Meteor.npmRequire("user-agent-parser")
 const device = Meteor.npmRequire("user-agent-device-mapper")
 const geoip = Meteor.npmRequire("geoip-lite")
 const cors = Meteor.npmRequire("cors")
 const helmet = Meteor.npmRequire("helmet")
 
-Picker.middleware(cors({
-  origin(origin, done) {
-    const isWhiteListed = !!Domains.find(
-      {domain: origin},
-      {fields: {domains: 1}}
-    ).count()
-    done(null, isWhiteListed)
-  }
-}))
+Picker.middleware(cors())
 Picker.middleware(helmet())
+Picker.middleware((req, res, next) => {
+  const referer = url.parse(req.headers.referer)
+  const query = queryString.parse(req._parsedUrl.query)
+  const domain = referer.host || null
+  const _id = query.k || null
+  if (!!Domains.findOne({_id: _id, domain: domain})) {
+    next()
+  } else {
+    res.writeHead(412)
+    res.end()
+  }
+})
 
 Picker.route("/ping", (params, req, res, next) => {
   const headers = req.headers
   const query = params.query
   const ip = headers["x-forwarded-for"]
-  const geo = geoip.lookup(ip)
+  const geo = geoip.lookup(ip) || {}
   const agent = userAgent(headers["user-agent"])
   const path = url.parse(req.headers.referer).pathname
   agent.device.type = device.getDeviceType(headers["user-agent"])
   Visits.insert({
-    dkey: query.k,
+    dkey: query.k || null,
     path: path,
     ip: ip,
     geo: {
-      ll: geo.ll,
-      c: geo.country,
-      ct: geo.city,
-      rg: geo.region
+      ll: geo.ll || null,
+      c: geo.country || null,
+      ct: geo.city || null,
+      rg: geo.region || null
     },
     ua: agent,
-    lang: query.l,
+    lang: query.l || null,
     screen: {
-      w: +query.w,
-      h: +query.h,
+      w: +query.w || null,
+      h: +query.h || null,
     }
   })
   res.writeHead(204)
