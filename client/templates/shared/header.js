918b186713bd5b8679a97767338f59f5295bf676
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-03-18 13:10:22 -0300

    improve navbar icons

--- client/templates/shared/header.js
+++ client/templates/shared/header.js
@@ -1,10 +1,3 @@
-Template.header.helpers({
-  isActive(template) {
-    const currentPath = FlowRouter.current().path
-    return currentPath.includes(template)
-  }
-})
-
 Template.header.events({
   "click a[data-logout]": () => {
     AccountsTemplates.logout()
