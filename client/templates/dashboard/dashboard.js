1498f6100442ecef9f63ca42b0fa11407a0810f7
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-03-17 21:11:56 -0300

    added nvd3 and added visitors x returned visitors graph

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -1,10 +1,36 @@
 Template.dashboard.onCreated(() => {
   Filter.init()
+})
+
+Template.dashboard.onRendered(() => {
+  const chart = nv.models.pieChart()
+    .x((data) => data.label)
+    .y((data) => data.value)
+    .noData("No data in this period")
+    .showLegend(false)
+    .showLabels(true)
   Tracker.autorun(() => {
-    const domainId = Session.get("domainId")
-    const startTime = Session.get("startTime")
-    const endTime = Session.get("endTime")
-    Filter.refresh(domainId, startTime, endTime)
+    if (Session.get("isReady")) {
+      const visits = Filter.getVisits()
+      const returnedVisits = _.chain(visits)
+        .filter((v) => {return !!v.rv})
+        .countBy("rv")
+        .toArray()
+        .reduce((sum, val) => {return sum + val}, 0)
+        .value()
+      const newVisits = _.size(visits) - returnedVisits
+      const data = [
+        {label: "New visitors", value: newVisits, color: "#2ecc71"},
+        {label: "Returned visitors", value: returnedVisits, color: "#2980b9"}
+      ]
+      nv.addGraph(() => {
+        d3.select("svg#nxrchart")
+          .datum(data)
+          .call(chart)
+        nv.utils.windowResize(chart.update)
+        return chart
+      })
+    }
   })
 })
 
@@ -18,7 +44,7 @@ Template.dashboard.helpers({
       .chain(visits)
       .map((value) => { return {name: value.ua.browser.name || "(not set)" }})
       .countBy("name")
-      .map((value, key) => { return {name: key, total: value} })
+      .map((value, key) => { return {name: key, total: value}})
       .sortBy("total")
       .reverse()
       .first(Filter.MAX)
