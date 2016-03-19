653486078c0072f0ae027b943d1c0de691fe2b70
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-03-19 11:50:01 -0300

    fix navbar active page

--- client/templates/shared/header.js
+++ client/templates/shared/header.js
@@ -1,3 +1,9 @@
+Template.header.helpers({
+  isActive(template) {
+    return FlowRouter.current().path.includes(template)
+  }
+})
+
 Template.header.events({
   "click a[data-logout]": () => {
     AccountsTemplates.logout()
