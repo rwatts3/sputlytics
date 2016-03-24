959bcb487bba293767195b818bffd12f01c9292b
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-03-24 14:59:57 -0300

    simplifying clipboard bind

--- client/templates/settings/_btnClipboard.js
+++ client/templates/settings/_btnClipboard.js
@@ -1,14 +1,13 @@
-Template._btnClipboard.onRendered(function() {
+Template._btnClipboard.onRendered(() => {
 	const id = this.data.id
   const clipboard = new Clipboard(`#${id}`)
-
   clipboard.on('success', (e) => {
-  	this.$(`#${id}`).tooltip('show')
+  	$(`#${id}`).tooltip('show')
 	})
 })
 
 Template._btnClipboard.events({
-  "mouseleave .btn-clipboard": (event, templateInstance) => {
-    this.$(`#${templateInstance.data.id}`).tooltip('hide')
+  "mouseleave .btn-clipboard": (event) => {
+    $(event.currentTarget).tooltip('hide')
   }
 })
