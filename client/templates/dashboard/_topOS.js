a4aca0cf58cef4d9877398964dfff87a51c8ee8c
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-03-28 18:59:51 -0300

    Added OS services to refactoring os table

--- client/templates/dashboard/_topOS.js
+++ client/templates/dashboard/_topOS.js
@@ -1,14 +1,7 @@
 Template._topOS.helpers({
   operationSystems() {
     const visits = Filter.getVisits()
-    return _
-      .chain(visits)
-      .map((value) => { return {name: value.ua.os.name || "(not set)" }})
-      .countBy("name")
-      .map((value, key) => { return {name: key, total: value} })
-      .sortBy("total")
-      .reverse()
-      .first(Filter.MAX)
-      .value()
+    const oss = OS.filter(visits).reverse()
+    return _.first(oss, Filter.MAX)
   }
 })
