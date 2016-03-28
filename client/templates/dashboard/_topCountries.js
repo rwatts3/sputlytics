98f9110f963bd146422cfbe0e8f37b11adc5e94d
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-03-28 19:01:34 -0300

    Added Countries services to refactoring countries table

--- client/templates/dashboard/_topCountries.js
+++ client/templates/dashboard/_topCountries.js
@@ -1,14 +1,7 @@
 Template._topCountries.helpers({
   countries() {
     const visits = Filter.getVisits()
-    return _
-      .chain(visits)
-      .map((value) => { return {name: value.geo.c || "(not set)" }})
-      .countBy("name")
-      .map((value, key) => { return {name: key, total: value} })
-      .sortBy("total")
-      .reverse()
-      .first(Filter.MAX)
-      .value()
+    const countries = Countries.filter(visits).reverse()
+    return _.first(countries, Filter.MAX)
   }
 })
