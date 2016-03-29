5f11d0994b810639ad9faa4407c32ea226289b04
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-03-28 21:06:36 -0300

    refactoring topcountries to render reports

--- client/templates/dashboard/_topCountries.js
+++ client/templates/dashboard/_topCountries.js
@@ -1,7 +1,7 @@
 Template._topCountries.helpers({
   countries() {
-    const visits = Filter.getVisits()
-    const countries = CountryService.filter(visits).reverse()
-    return _.first(countries, Filter.MAX)
+    const reports = Filter.getReports()
+    const countries = CountryService.group(reports)
+    return _.first(countries.reverse(), Filter.MAX)
   }
 })
