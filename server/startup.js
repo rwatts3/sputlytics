5d3688335ee829fa7313f668fa28a88dde5d351d
lnmunhoz <ln.munhoz@gmail.com>
2016-04-08 02:33:51 -0300

    Remove seed from startup

--- server/startup.js
+++ server/startup.js
@@ -1,8 +1,8 @@
 // Startup code
 Meteor.startup(() => {
   console.log("Sputlytics running...")
-  Seed.clear();
-  Seed.init();
+  // Seed.clear();
+  // Seed.init();
   SyncedCron.config({ collectionTTL: 172800 })
   SyncedCron.start()
 })
