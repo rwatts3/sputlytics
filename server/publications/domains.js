c98f056c38ad789cf71010e6a54c31761bdf428e
lnmunhoz <ln.munhoz@gmail.com>
2016-04-08 02:05:53 -0300

    Fix publication with composition

--- server/publications/domains.js
+++ server/publications/domains.js
@@ -1,10 +1,21 @@
 import { Domains } from '../../imports/api/domains.js'
 
-Meteor.publish("domains", function() {
+Meteor.publishComposite('domains', function() {
   if (this.userId) {
-    const user = Meteor.users.findOne(this.userId, { fields: { domainIds: 1 } })
-    return Domains.find({ _id: { $in: user.domainIds } })
-  } else {
-    throw new Meteor.Error(412, "Not allowed")
+    return {
+      find() {
+        return Meteor.users.find({ _id: this.userId }, {
+          fields: {
+            domainIds: 1
+          }
+        })
+      },
+      children: [{
+        find(user) {
+          return Domains.find({ _id: { $in: user.domainIds } })
+        }
+      }]
+    }
   }
+  throw new Meteor.Error(412, "Not allowed")
 })
