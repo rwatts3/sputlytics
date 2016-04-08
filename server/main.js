b2a1be33b8395c7d3ba487f9fbabd05b3f21b0ed
lnmunhoz <ln.munhoz@gmail.com>
2016-04-08 02:30:01 -0300

    Move startup code

--- server/main.js
+++ server/main.js
@@ -1,5 +1,1 @@
-// Startup code
-DBIndex.init()
-SyncedCron.config({ collectionTTL: 172800 })
-SyncedCron.start()
-console.log("Sputlytics running...")
+// Imports here...
--- server/startup.js
+++ server/startup.js
@@ -1,1 +1,9 @@
+// Startup code
+Meteor.startup(() => {
+  console.log("Sputlytics running...")
+  Seed.clear();
+  Seed.init();
+  SyncedCron.config({ collectionTTL: 172800 })
+  SyncedCron.start()
+})
 
